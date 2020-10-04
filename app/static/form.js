6d036cfc9571956bb14b3a896adde419a71f21de
Jake Stockwin <jstockwin@gmail.com>
2020-10-04 11:20:26 +0100

    Initial project setup and WIP prototype

---
+++ app/static/form.js
@@ -0,0 +1,97 @@
+$("#form").alpaca({
+  schema: {
+    type: "object",
+    title: "Brewing Recipe",
+    properties: {
+      malts: {
+        type: "array",
+        title: "Malts",
+        items: {
+          type: "object",
+          properties: {
+            name: {
+              title: "Name",
+              type: "string",
+            },
+            quantity: {
+              title: "Quantity (kg)",
+              type: "number",
+            },
+            specific_gravity: {
+              title: "Specific Gravity",
+              type: "number",
+            },
+          },
+        },
+      },
+      hops: {
+        type: "array",
+        title: "Hops",
+        items: {
+          type: "object",
+          properties: {
+            name: {
+              title: "Name",
+              type: "string",
+            },
+            quantity: {
+              title: "Quantity (oz)",
+              type: "number",
+            },
+            alpha: {
+              title: "Alpha",
+              type: "number",
+            },
+            boil_time: {
+              title: "Boil Time (mins)",
+              type: "number",
+            },
+          },
+        },
+      },
+      yeast: {
+        type: "object",
+        title: "Yeast",
+        properties: {
+          name: {
+            title: "Name",
+            type: "string",
+          },
+          attenuation: {
+            title: "Attenuation (%)",
+            type: "number",
+            minimum: 0,
+            maximum: 100,
+          },
+        },
+      },
+      expected_mash_efficiency: {
+        type: "number",
+        title: "Expected Mash Efficiency (%)",
+        minimum: 0,
+        maximum: 100,
+      },
+      post_boil_volume: {
+        type: "number",
+        title: "Expected post-boil volume (L)",
+        minimum: 0,
+      },
+    },
+  },
+  options: {
+    fields: {
+      malts: {
+        toolbarSticky: true,
+        toolbar: { showLabels: true },
+        actionbar: { showLabels: true },
+      },
+    },
+  },
+  postRender: function (control) {
+    for (i = 0; i < control.children.length; i++) {
+      control.children[i].on("change", function () {
+        on_change();
+      });
+    }
+  },
+});
---
+++ app/static/on_change.js
@@ -0,0 +1,43 @@
+var on_change = function () {
+  var data = $("#form").alpaca("get").getValue();
+  // Calculate original gravity
+  var malts = data.malts;
+  var gravity_points = 0;
+  for (var i = 0; i < malts.length; i++) {
+    var malt = malts[i];
+    gravity_points +=
+      (malt.specific_gravity * 1000 - 1000) * malt.quantity * 2.20462262185;
+  }
+  var mash_efficiency = data.expected_mash_efficiency / 100;
+  var post_boil_volume_gallons = data.post_boil_volume * 0.219969;
+  var original_gravity_points =
+    (gravity_points * mash_efficiency) / post_boil_volume_gallons;
+  var original_gravity = (original_gravity_points + 1000) / 1000;
+  if (original_gravity > 0) {
+    console.log("OG: " + original_gravity);
+  }
+  // Calculate final gravity
+  var final_gravity =
+    1 + (original_gravity * (1 - data.yeast.attenuation / 100)) / 1000;
+  if (final_gravity) {
+    console.log("FG: " + final_gravity);
+  }
+  // Calculate IBU
+  // TODO: Boil time is the actual time that it boils for. We should actually ask
+  // for the total boil time and the addition time, and then calculate the boil
+  // time.
+  var hops = data.hops;
+  var bigness_factor = 1.65 * 0.000125 ** (original_gravity - 1);
+  var ibu = 0;
+  for (var i = 0; i < hops.length; i++) {
+    var hop = hops[i];
+    var boil_time_factor = (1 - Math.exp(-0.04 * hop.boil_time)) / 4.15;
+    var utilisation = bigness_factor * boil_time_factor;
+    ibu +=
+      (utilisation * ((hop.alpha / 100) * hop.quantity * 7489)) /
+      post_boil_volume_gallons;
+  }
+  if (ibu) {
+    console.log("IBU: " + ibu);
+  }
+};
